ed903049dd29a91df8142afe2b78ce328182bd52
chadhas282-afk <chadhas282@gmail.com>
2026-02-27 20:55:08 +0530

    add logic to language button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ exchageIcon = document.querySelector(".exchange"),
 selectTag = document.querySelectorAll("select"),
 icons = document.querySelectorAll(".row i");
 translateBtn = document.querySelector("button"),
+
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
         let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";

411daa40262cba8f94c2f15c7e4757cc91c502ba
chadhas282-afk <chadhas282@gmail.com>
2026-02-27 20:51:28 +0530

    add logic to icon

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ selectTag.forEach((tag, id) => {
         tag.insertAdjacentHTML("beforeend", option);
     }
 });
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
